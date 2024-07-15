785f4ca2f51620a2e5dd2578f418187ed7df0b90
Akinwumi Iyanuoluwa Ayomiposi <akinwumikaliyanu@gmail.com>
2024-07-15 01:23:29 +0100

    started rust api project

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+
+#[macro_use]
+extern crate rocket;
+
+
+
+#[get("/")]
+fn index() -> String{
+    String::from("Hello, world")
+}
+
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes!(index))
+}
